e03fc8e59ce7e7f9d12b6def59eaac6b36da289e
BrunoAmerio <88524640+BrunoAmerio@users.noreply.github.com>
2022-02-14 21:11:57 -0300

    Routes

--- api/db.js
+++ api/db.js
@@ -1,5 +1,27 @@
-const PersonalTrainer = require('./models/Client');
+require('dotenv').config(); 
+const { Sequelize } = require('sequelize');
+const {DB_USER, DB_PASSWORD, DB_HOST,} = process.env;
+const fs = require('fs');
+const path = require('path');
+const { dirname, basename } = require('path');
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/trainingapp`,{logging:false});
+//Verificamos que se haya conectado 
+sequelize.authenticate()
+      .then(()=> console.log('Conection Success...'))
+      .catch(err => console.log('Error in connection: ',err))
+
+//Definimos los modelos
+const dirName = path.basename(__filename);
+const modelDefiners = [];
+
+fs.readdirSync(path.join(__dirname, '/models'))
+      .filter((file) => (file.indexOf('.') !== 0) && (file !== dirName) && (file.slice(-3) === '.js'))
+      .forEach((file) => {
+            modelDefiners.push(require(path.join(__dirname, '/models', file)));
+      });
+modelDefiners.forEach(model => model(sequelize));
+console.log(sequelize.models);
 
 
-
-sequelize.define()
+module.exports = sequelize;
