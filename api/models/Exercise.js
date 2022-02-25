be5affdfbd9261180d785134a6bbbcc1d916e1c4
Manu10mza <gerardo_romero@outlook.com.ar>
2022-02-25 15:57:32 -0300

    updated models add disabled

--- api/models/Exercise.js
+++ api/models/Exercise.js
@@ -1,25 +1,30 @@
 const { DataTypes } = require("sequelize");
 
-const Exercise = (sequelize)=>{
-      sequelize.define('Exercise',{
-            id:{
-                  type: DataTypes.UUID,
-                  defaultValue: DataTypes.UUIDV4,
-                  primaryKey:true
-            },
-            title:{
-                  type: DataTypes.STRING,
-                  allowNull: false
-            },
-            description:{
-                  type: DataTypes.TEXT,
-                  allowNull: false
-            },
-            video:{
-                  type: DataTypes.STRING,
-                  allowNull: true
-            }
-      })
-}
+const Exercise = (sequelize) => {
+  sequelize.define("Exercise", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    video: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    disabled: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: true,
+    },
+  });
+};
 
 module.exports = Exercise;
