493676be9ee958167d2447db6f0267655abeb4f7
heredia lucas <heredialucasfac22@hotmail.com>
2022-03-02 21:11:07 -0300

    asd

--- api/routes/Routine.js
+++ api/routes/Routine.js
@@ -16,6 +16,7 @@ router.post("/:ownerId", async (req, res) => {
         id: ownerId,
       },
     });
+    console.log(user)
 
     //Se verifica si falta algun dato necesario
     if (!title || !exercises || !ownerId || !price) {
